442e9303801c16640f6f2918ecbe841b5ded41fc
Amir Yasin <ayasin@2u.com>
2016-08-25 08:32:04 -0500

    activate movie routes

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const memberRoutes = require('./routes/members.js');
 const movieRoutes = require('./routes/movies.js');
 
 const app = express();
-app..use(bodyParser.json());
+app.use(bodyParser.json());
 
 memberRoutes(app, '/api/member', dataStore);
-//movieRoutes(app, dataStore);
+movieRoutes(app, '/api/movie', dataStore);
 
 app.listen(3300, () => {
   console.log('Movie store listening on 3300');
