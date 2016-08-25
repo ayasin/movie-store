2d956bb4bc9475c64f601f920e60ec75809977a0
Amir Yasin <ayasin@datatensor.com>
2016-08-24 23:02:03 -0500

    WIP

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const dataStore = require('./initial.state.js');
 const memberRoutes = require('./routes/members.js');
 const movieRoutes = require('./routes/movies.js');
 
 const app = express();
+app..use(bodyParser.json());
 
 memberRoutes(app, '/api/member', dataStore);
 //movieRoutes(app, dataStore);
--- routes/members.js
+++ routes/members.js
@@ -1,5 +1,27 @@
 module.exports = (app, path, dataStore) => {
   app.get(`${path}`, (req, res) => {
-    res.status(200).json(dataStore.members);
+    res.status(200).json({status: 'ok', members: dataStore.members});
+  });
+
+  app.get(`${path}/:id`, (req, res) => {
+    const idx = req.params.id;
+    if (idx < 0 || idx > dataStore.members.length) {
+      return res.status(404).json({status: 'error', error: 'Member not found'});
+    }
+    res.status(200).json({status: 'ok', member: dataStore.members[idx]});
+  });
+
+  app.post(`${path}`, (req, res) => {
+    dataStore.members.push(req.body);
+    res.status(200).json({status: 'ok'});
+  });
+
+  app.put(`${path}/:id`, (req, res) => {
+    const idx = req.params.id;
+    if (idx < 0 || idx > dataStore.members.length) {
+      return res.status(404).json({status: 'error', error: 'Member not found'});
+    }
+    dataStore.members[idx] = req.body;
+    res.status(200).json({status: 'ok'});
   });
 }
